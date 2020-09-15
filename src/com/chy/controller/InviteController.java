33ff23809920ea350ffa005a433172558fa48cf9
licent <736912754@qq.com>
2020-09-15 22:38:16 +0800

    qrcode

---
+++ src/com/chy/controller/InviteController.java
@@ -0,0 +1,89 @@
+package com.chy.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.alibaba.fastjson.JSONObject;
+import com.chy.pojo.out.User;
+import com.tools.MyHttpSender;
+import com.tools.ResponseCode;
+import com.tools.ResponseInfo;
+import com.tools.Tools;
+
+import sun.misc.BASE64Encoder;
+
+/**
+ * @邀请二维码生成
+ * */
+@Controller
+public class InviteController {
+	
+	@ResponseBody
+	@RequestMapping("/manage/invite/inviteFactory")
+	public String inviteFactory(@RequestParam Map<String, Object> params, HttpServletRequest request) {
+		ResponseInfo<String> info = new ResponseInfo<String>();
+		try {
+			
+			if(params.get("userId")==null) {
+				info.setCode(ResponseCode.SUCC);
+				info.setMsg("参数缺失userId");
+				return info.toJsonString();
+			}
+			//user.getId()
+			String urlTail="/pages/aboutus_firm_apply_for/aboutus_firm_apply_for?parentId="+Tools.ObjectToInt(params.get("userId"));
+			
+		    String result =MyHttpSender.commonGet(MyHttpSender.GET_ACCESS_TOKEN, "grant_type=client_credential&appid=wxce31758573e47ce3&secret=50745ba9b3b68f32a005a14ecf438ec5");
+		    JSONObject ja =JSONObject.parseObject(result);
+		    
+		    Map<String,String> pa =new HashMap<String, String>();
+		    pa.put("path", urlTail);
+		    String base64=MyHttpSender.commonPostJsonParams(MyHttpSender.GET_QR_CODE+"?access_token="+ja.getString("access_token"), pa);
+			info.setCode(ResponseCode.SUCC);
+			info.setMsg(base64);
+			info.setData(base64);
+			return info.toJsonString();
+		} catch (Exception e) {
+			e.printStackTrace();
+			info.setCode(ResponseCode.EXCEPTION);
+			return info.toJsonString();
+		}
+	}
+	
+	@ResponseBody
+	@RequestMapping("/manage/invite/inviteAutoPoint")
+	public String inviteAutoPoint(@RequestParam Map<String, Object> params, HttpServletRequest request) {
+		ResponseInfo<String> info = new ResponseInfo<String>();
+		try {
+			
+			if(params.get("userId")==null) {
+				info.setCode(ResponseCode.SUCC);
+				info.setMsg("参数缺失userId");
+				return info.toJsonString();
+			}
+			//user.getId()
+			String urlTail="/pages/shopowner/apply?parentId="+Tools.ObjectToInt(params.get("userId"));
+			
+		    String result =MyHttpSender.commonGet(MyHttpSender.GET_ACCESS_TOKEN, "grant_type=client_credential&appid=wxce31758573e47ce3&secret=50745ba9b3b68f32a005a14ecf438ec5");
+		    JSONObject ja =JSONObject.parseObject(result);
+		    
+		    Map<String,String> pa =new HashMap<String, String>();
+		    pa.put("path", urlTail);
+		    String base64=MyHttpSender.commonPostJsonParams(MyHttpSender.GET_QR_CODE+"?access_token="+ja.getString("access_token"), pa);
+			info.setCode(ResponseCode.SUCC);
+			info.setData(base64);
+			info.setMsg(base64);
+			return info.toJsonString();
+		} catch (Exception e) {
+			e.printStackTrace();
+			info.setCode(ResponseCode.EXCEPTION);
+			return info.toJsonString();
+		}
+	}
+}
--- src/com/tools/MyHttpSender.java
+++ src/com/tools/MyHttpSender.java
@@ -2,9 +2,12 @@ package com.tools;
 
 import static com.tools.WXPayConstants.USER_AGENT;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
+import javax.imageio.stream.FileImageOutputStream;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -27,26 +30,32 @@ import org.apache.http.impl.conn.BasicHttpClientConnectionManager;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
+import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
+
 public class MyHttpSender {
-	public static String UPLOAD_URL="https://api.weixin.qq.com/sns/jscode2session";
-	
-	public static String MSG_CODE_SEND_URL="https://3ke.xyz/api/ali/sms";
-	
-	public static String WE_CHAT_PAY="https://api.mch.weixin.qq.com/pay/unifiedorder";
+	public static String UPLOAD_URL = "https://api.weixin.qq.com/sns/jscode2session";
+
+	public static String MSG_CODE_SEND_URL = "https://3ke.xyz/api/ali/sms";
+
+	public static String WE_CHAT_PAY = "https://api.mch.weixin.qq.com/pay/unifiedorder";
+
+	public static String GET_ACCESS_TOKEN = "https://api.weixin.qq.com/cgi-bin/token";
+
+	public static String GET_QR_CODE = "https://api.weixin.qq.com/cgi-bin/wxaapp/createwxaqrcode";
 
 	/**
-	 * @图片上传公共接口调用 
-	 * */
-	public static String commonGet(String u,String params) throws Exception {
+	 * @图片上传公共接口调用
+	 */
+	public static String commonGet(String u, String params) throws Exception {
 		String strResult = "9";
 		try {
-			String url = u +"?" + params;
+			String url = u + "?" + params;
 			System.out.println(url);
 			CloseableHttpClient httpClient = HttpClients.createDefault();
 			HttpGet httpGet = new HttpGet(url);
 			CloseableHttpResponse response = httpClient.execute(httpGet);
 			if (response != null && response.getStatusLine().getStatusCode() == 200) {
-				HttpEntity entity = response.getEntity(); 
+				HttpEntity entity = response.getEntity();
 				strResult = EntityUtils.toString(entity, "UTF-8");
 			} else {
 				strResult = "404";
@@ -62,31 +71,29 @@ public class MyHttpSender {
 		}
 		return strResult;
 	}
-	
-	
+
 	/**
 	 * post公共接口
-	 * */
-	public static String commonPost(String u,Map<String,String> params) throws Exception {
+	 */
+	public static String commonPost(String u, Map<String, String> params) throws Exception {
 		String strResult = "9";
 		try {
 			String url = u;
 			System.out.println(url);
 			CloseableHttpClient httpClient = HttpClients.createDefault();
 			HttpPost httpPost = new HttpPost(url);
-			
-			
-			List<NameValuePair> parameters = new ArrayList<NameValuePair>();
-	        for (Map.Entry<String, String> entry : params.entrySet()) {
-	            parameters.add(new BasicNameValuePair(entry.getKey(), entry.getValue()));
-	        }
 
-			UrlEncodedFormEntity formEntity = new UrlEncodedFormEntity(parameters,"UTF-8");
+			List<NameValuePair> parameters = new ArrayList<NameValuePair>();
+			for (Map.Entry<String, String> entry : params.entrySet()) {
+				parameters.add(new BasicNameValuePair(entry.getKey(), entry.getValue()));
+			}
+
+			UrlEncodedFormEntity formEntity = new UrlEncodedFormEntity(parameters, "UTF-8");
 			httpPost.setEntity(formEntity);
-				
+
 			CloseableHttpResponse response = httpClient.execute(httpPost);
 			if (response != null && response.getStatusLine().getStatusCode() == 200) {
-				HttpEntity entity = response.getEntity(); 
+				HttpEntity entity = response.getEntity();
 				strResult = EntityUtils.toString(entity, "UTF-8");
 			} else {
 				strResult = "404";
@@ -102,45 +109,86 @@ public class MyHttpSender {
 		}
 		return strResult;
 	}
-	
-	
+
+	/**
+	 * post公共接口
+	 */
+	public static String commonPostJsonParams(String u, Map<String, String> params) throws Exception {
+		String base64str="";
+		try {
+			
+			String url = u;
+			System.out.println(url);
+			CloseableHttpClient httpClient = HttpClients.createDefault();
+			HttpPost httpPost = new HttpPost(url);
+
+			StringEntity requestEntity = new StringEntity(Tools.ObjectToJsonString(params), "utf-8");
+			requestEntity.setContentEncoding("UTF-8");
+			httpPost.setHeader("Content-type", "application/json");
+
+			httpPost.setEntity(requestEntity);
+
+
+			CloseableHttpResponse response = httpClient.execute(httpPost);
+			if (response != null && response.getStatusLine().getStatusCode() == 200) {
+				HttpEntity entity = response.getEntity();
+				byte [] strResult = EntityUtils.toByteArray(entity);
+				
+				//FileImageOutputStream imageOutput = new FileImageOutputStream(new File("D:\\qrCode.png"));// 打开输入流
+				
+				base64str = Base64.encode(strResult);
+//
+//				imageOutput.write(strResult, 0, strResult.length);// 将byte写入硬盘
+//
+//				imageOutput.close();
+			}
+			if (response != null) {
+				response.close();
+			}
+			if (httpClient != null) {
+				httpClient.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return base64str;
+		
+	}
+
 	/**
 	 * post公共 xml参数
-	 * */
-	public static String commonPost(String u,String xmlString,String MchId,int readTimeoutMs,int connectTimeoutMs) throws Exception {
+	 */
+	public static String commonPost(String u, String xmlString, String MchId, int readTimeoutMs, int connectTimeoutMs)
+			throws Exception {
 		String strResult = "9";
 		try {
 			BasicHttpClientConnectionManager connManager = new BasicHttpClientConnectionManager(
-                    RegistryBuilder.<ConnectionSocketFactory>create()
-                            .register("http", PlainConnectionSocketFactory.getSocketFactory())
-                            .register("https", SSLConnectionSocketFactory.getSocketFactory())
-                            .build(),
-                    null,
-                    null,
-                    null
-            );
-			 HttpClient httpClient = HttpClientBuilder.create()
-		                .setConnectionManager(connManager)
-		                .build();
+					RegistryBuilder.<ConnectionSocketFactory>create()
+							.register("http", PlainConnectionSocketFactory.getSocketFactory())
+							.register("https", SSLConnectionSocketFactory.getSocketFactory()).build(),
+					null, null, null);
+			HttpClient httpClient = HttpClientBuilder.create().setConnectionManager(connManager).build();
 
-		        HttpPost httpPost = new HttpPost(u);
+			HttpPost httpPost = new HttpPost(u);
 
-		        RequestConfig requestConfig = RequestConfig.custom().setSocketTimeout(readTimeoutMs).setConnectTimeout(connectTimeoutMs).build();
-		        httpPost.setConfig(requestConfig);
+			RequestConfig requestConfig = RequestConfig.custom().setSocketTimeout(readTimeoutMs)
+					.setConnectTimeout(connectTimeoutMs).build();
+			httpPost.setConfig(requestConfig);
 
-		        StringEntity postEntity = new StringEntity(xmlString, "UTF-8");
-		        httpPost.addHeader("Content-Type", "text/xml");
-		        httpPost.addHeader("User-Agent", USER_AGENT + " " + MchId);
-		        httpPost.setEntity(postEntity);
+			StringEntity postEntity = new StringEntity(xmlString, "UTF-8");
+			httpPost.addHeader("Content-Type", "text/xml");
+			httpPost.addHeader("User-Agent", USER_AGENT + " " + MchId);
+			httpPost.setEntity(postEntity);
 
-		        HttpResponse httpResponse = httpClient.execute(httpPost);
-		        HttpEntity httpEntity = httpResponse.getEntity();
-		        return EntityUtils.toString(httpEntity, "UTF-8");
-		        
+			HttpResponse httpResponse = httpClient.execute(httpPost);
+			HttpEntity httpEntity = httpResponse.getEntity();
+			return EntityUtils.toString(httpEntity, "UTF-8");
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return strResult;
 	}
-	
+
 }
